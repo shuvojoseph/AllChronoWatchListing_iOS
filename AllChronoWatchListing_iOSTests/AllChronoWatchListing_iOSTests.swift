22dea1961d70f8b829173460dac9ae4ee294b08c
shuvojoseph <shuvo.ewu.cse@gmail.com>
2026-09-01 14:16:48 +0600

    Initial Commit

---
+++ AllChronoWatchListing_iOSTests/AllChronoWatchListing_iOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  AllChronoWatchListing_iOSTests.swift
+//  AllChronoWatchListing_iOSTests
+//
+//  Created by Shuvo Joseph on 1/9/26.
+//
+
+import Testing
+
+struct AllChronoWatchListing_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
